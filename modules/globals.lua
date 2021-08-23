3dabd6fe744b5f32da9856975c2176d7b526f678
Polar <drewboyne@hotmail.com>
2021-08-23 14:39:35 +0100

    Add spawn chances for each rod's fish table

--- modules/globals.lua
+++ modules/globals.lua
@@ -44,36 +44,68 @@ fishing_rods = {
     ["rod0"] = {    name = "Wooden Rod", tooltip = "Grandad's old wooden fishing rod", cost = 100,
                     line_length = 60, catch_time = 10, catch_chance = 4, fish_chance = 30,
                     available_fish = {
-                        { id = "fish0", chance_s = 0, chance_e = 10},
-                        { id = "fish1", chance_s = 11, chance_e = 100}
+                        { id = "fish0", chance_s = 0, chance_e = 40}, -- Guppy *
+                        { id = "fish1", chance_s = 41, chance_e = 70}, -- Sardine *
+                        { id = "fish5", chance_s = 71, chance_e = 80}, -- Crab **
+                        { id = "fish6", chance_s = 81, chance_e = 100} -- Prawn *
                     }};
 
     ["rod1"] = {    name = "Lightweight Rod", tooltip = "Modern aluminium fishing rod", cost = 1000,
                     line_length = 75, catch_time = 12, catch_chance = 6, fish_chance = 40,
                     available_fish = {
-                        { id = "fish0", chance_s = 0, chance_e = 10 },
-                        { id = "fish1", chance_s = 11, chance_e = 100 }
+                        { id = "fish0", chance_s = 0, chance_e = 20 }, -- Guppy
+                        { id = "fish1", chance_s = 21, chance_e = 40 }, -- Sardine
+                        { id = "fish4", chance_s = 41, chance_e = 60}, -- Mackerel *
+                        { id = "fish5", chance_s = 61, chance_e = 80}, -- Crab
+                        { id = "fish6", chance_s = 81, chance_e = 95}, -- Prawn
+                        { id = "fish7", chance_s = 96, chance_e = 100} -- Lobster **
                     }};
 
     ["rod2"] = {    name = "Diamond Rod", tooltip = "Premium, diamond standard fishing rod", cost = 10000,
                     line_length = 100, catch_time = 15, catch_chance = 8, fish_chance = 50,
                     available_fish = {
-                        { id = "fish0", chance_s = 0, chance_e = 10 },
-                        { id = "fish1", chance_s = 11, chance_e = 100 }
+                        { id = "fish0", chance_s = 0, chance_e = 15 }, -- Guppy
+                        { id = "fish1", chance_s = 16, chance_e = 30 }, -- Sardine
+                        { id = "fish2", chance_s = 31, chance_e = 40 }, -- Octopus *
+                        { id = "fish3", chance_s = 41, chance_e = 50 }, -- Sea Snake *
+                        { id = "fish4", chance_s = 51, chance_e = 60}, -- Mackerel
+                        { id = "fish5", chance_s = 61, chance_e = 70}, -- Crab
+                        { id = "fish6", chance_s = 71, chance_e = 80}, -- Prawn
+                        { id = "fish7", chance_s = 81, chance_e = 95}, -- Lobster
+                        { id = "fish8", chance_s = 96, chance_e = 100} -- Pufferfish **
                     }};
 
     ["rod3"] = {    name = "Uranium Rod", tooltip = "Uranium enriched fishing rod", cost = 20000,
                     line_length = 125, catch_time = 18, catch_chance = 10, fish_chance = 60,
                     available_fish = {
-                        { id = "fish0", chance_s = 0, chance_e = 10 },
-                        { id = "fish1", chance_s = 11, chance_e = 100 }
+                        { id = "fish0", chance_s = 0, chance_e = 10 }, -- Guppy
+                        { id = "fish1", chance_s = 11, chance_e = 20 }, -- Sardine
+                        { id = "fish2", chance_s = 21, chance_e = 30 }, -- Octopus
+                        { id = "fish3", chance_s = 31, chance_e = 40 }, -- Sea Snake
+                        { id = "fish4", chance_s = 41, chance_e = 50}, -- Mackerel
+                        { id = "fish5", chance_s = 51, chance_e = 60}, -- Crab
+                        { id = "fish6", chance_s = 61, chance_e = 70}, -- Prawn
+                        { id = "fish7", chance_s = 71, chance_e = 80}, -- Lobster
+                        { id = "fish8", chance_s = 81, chance_e = 90}, -- Pufferfish
+                        { id = "fish9", chance_s = 91, chance_e = 95}, -- Glowfish **
+                        { id = "fish10", chance_s = 96, chance_e = 100} -- Lionfish **
                     }};
 
     ["rod4"] = {    name = "Rainbow Rod", tooltip = "Nanobii's legendary fishing rod", cost = 50000,
                     line_length = 150, catch_time = 20, catch_chance = 15, fish_chance = 70,
                     available_fish = {
-                        { id = "fish0", chance_s = 0, chance_e = 40 },
-                        { id = "fish1", chance_s = 41, chance_e = 100 }
+                        { id = "fish0", chance_s = 0, chance_e = 5 }, -- Guppy
+                        { id = "fish1", chance_s = 6, chance_e = 10 }, -- Sardine
+                        { id = "fish2", chance_s = 11, chance_e = 20}, -- Octopus
+                        { id = "fish3", chance_s = 21, chance_e = 30 }, -- Sea Snake
+                        { id = "fish4", chance_s = 31, chance_e = 35}, -- Mackerel
+                        { id = "fish5", chance_s = 36, chance_e = 45}, -- Crab
+                        { id = "fish6", chance_s = 46, chance_e = 55}, -- Prawn
+                        { id = "fish7", chance_s = 56, chance_e = 65}, -- Lobster
+                        { id = "fish8", chance_s = 66, chance_e = 75}, -- Pufferfish
+                        { id = "fish9", chance_s = 76, chance_e = 85}, -- Glowfish
+                        { id = "fish10", chance_s = 86, chance_e = 95}, -- Lionfish
+                        { id = "fish11", chance_s = 96, chance_e = 100} -- Fossil **
                     }};
 };
 
